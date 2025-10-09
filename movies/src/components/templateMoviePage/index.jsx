21abfd4338c95dedf7ef658341fbdfbfc93ce4d7
JasonLiu <jasonliudehui227@gmail.com>
2025-10-09 19:14:08 +0100

    Updating movie page to use react query

--- movies/src/components/templateMoviePage/index.jsx
+++ movies/src/components/templateMoviePage/index.jsx
@@ -1,19 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MovieHeader from "../headerMovie";
 import Grid from "@mui/material/Grid";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { getMovieImages } from "../../api/tmdb-api";
 
-const TemplateMoviePage = ({ movie, children }) => {
-  const [images, setImages] = useState([]);
+import { useQuery } from "@tanstack/react-query";
+import Spinner from '../spinner'
 
-  useEffect(() => {
-    getMovieImages(movie.id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+const TemplateMoviePage = ({ movie, children }) => {
+ const { data, error, isPending, isError } = useQuery({
+    queryKey: ['images', { id: movie.id }],
+    queryFn: getMovieImages,
+  });
+
+  if (isPending) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  
+  const images = data.posters 
 
   return (
     <>
