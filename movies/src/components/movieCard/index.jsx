4598d79ade0eef83e7a03da949d21fbc4d46cd4b
JasonLiu <jasonliudehui227@gmail.com>
2025-10-10 10:15:21 +0100

    Card icons - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -16,7 +16,7 @@ import Avatar from '@mui/material/Avatar';
 import React, { useContext  } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 
-export default function MovieCard({movie}) {
+export default function MovieCard({ movie, action }) {
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
@@ -70,15 +70,16 @@ export default function MovieCard({movie}) {
           </Grid>
         </Grid>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
+           <CardActions disableSpacing>
+      
+        {action(movie)}
+      
         <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
           </Button>
         </Link>
+        
       </CardActions>
     </Card>
   );
