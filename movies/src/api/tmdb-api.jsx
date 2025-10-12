8120fc60cb5f4c7aead10d3dbc0e65b85840b5d6
JasonLiu <jasonliudehui227@gmail.com>
2025-10-12 01:51:02 +0100

    Added Upcoming movies page.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -86,3 +86,39 @@ export const getMovieImages = ({ queryKey }) => {
       throw error
    });
   };
+
+  export const getUpcomingMovie = ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const id = typeof idPart === "object" ? idPart.id : idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export const getUpcomingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -11,6 +11,8 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
 
 import AddMovieReviewPage from './pages/addMovieReviewPage'
+import UpcomingMoviesPage from './pages/upcomingMoviesPage'
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -34,7 +36,8 @@ const App = () => {
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={ <Navigate to="/" /> } />
-             <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+            <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+            <Route path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
